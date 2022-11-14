92013e32e23c868c34c8a4ae8a6b3aaad4506658
ThVinicius <vinicioss9955@gmail.com>
2022-11-14 17:27:54 -0300

    feat: logout

--- src/components/header/app/App.js
+++ src/components/header/app/App.js
@@ -1,17 +1,22 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useGlobal } from '../../../context/globalContext'
 import Logo from '../logo/Logo'
 import { Exit } from '../../../assets/icons/icons'
 import Input from '../../input/Input'
+import logout from '../../../utils/logout'
 import { Container, Logout, InputBox, Box } from './appStyles'
 
 export default function App() {
   const [search, setSearch] = useState('')
+  const { global } = useGlobal()
+  const navigate = useNavigate()
 
   return (
     <Container>
       <Box>
         <Logo />
-        <Logout>
+        <Logout onClick={() => logout(global, navigate)}>
           <Exit />
         </Logout>
       </Box>
