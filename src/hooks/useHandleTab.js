63213fad0a5e099a110cee9efbedad1e8a6d2cc5
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-03-12 11:35:27 -0300

    feat:  useHandleTab

---
+++ src/hooks/useHandleTab.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+export default function useHandleTab(tab, setSearch) {
+  const [placeholder, setPlaceholder] = useState('')
+  const [disabled, setDisabled] = useState(false)
+
+  console.log(tab)
+
+  useEffect(() => {
+    switch (tab) {
+      case 'DISCIPLINAS':
+        setPlaceholder('Pesquise por disciplina')
+        setDisabled(false)
+        break
+
+      case 'PESSOA INSTRUTORA':
+        setPlaceholder('Pesquise por pessoa instrutora')
+        setDisabled(false)
+        break
+
+      case 'ADICIONAR':
+        setSearch('')
+        setPlaceholder('Pesquisa desabilitada')
+        setDisabled(true)
+        break
+
+      default:
+        break
+    }
+  }, [tab])
+
+  return { placeholder, disabled }
+}
