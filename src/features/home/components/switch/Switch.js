31be73834cdebf48d7b19a9968f382e708de4f98
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-03-28 17:54:03 -0300

    feat: filter by teachers

--- src/features/home/components/switch/Switch.js
+++ src/features/home/components/switch/Switch.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useGlobal } from '../../../../context/globalContext'
 import useFilterByDisciplines from '../../../../hooks/useFilterByDisciplines'
+import useFilterByTeachers from '../../../../hooks/useFilterByTeachers'
 import { useTab } from '../../../../context/tabContext'
 import DisciplineAccordion from '../disciplineAccordion/DisciplineAccordion'
 import TeachersAccordion from '../teachersAccordion/TeachersAccordion'
@@ -10,6 +11,7 @@ import { Spinner } from '../../../../components/spinners/spinners'
 export default function Switch({ selected, search }) {
   const { global } = useGlobal()
   const { result } = useFilterByDisciplines(search)
+  const { teachers } = useFilterByTeachers(search)
   const { setTab } = useTab()
 
   useEffect(() => {
@@ -21,7 +23,7 @@ export default function Switch({ selected, search }) {
       return <DisciplineAccordion array={result} />
 
     case 'PESSOA INSTRUTORA':
-      return <TeachersAccordion array={global.testsByTeachers} />
+      return <TeachersAccordion array={teachers} />
 
     case 'ADICIONAR':
       return global.teachersDisciplines !== null ? <Form /> : Spinner
